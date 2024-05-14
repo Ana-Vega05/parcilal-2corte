e276d661d3f547c9054240befc396ff4915bacab
Dyelamo <dyelamo@unicesar.edu.co>
2024-05-13 20:25:16 -0500

    Interfacez de vista y Interfaz de repositorio (Luis Santiago)

---
+++ DATOS/IRepositorioProducto.cs
@@ -0,0 +1,26 @@
+﻿using ENTIDADES;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+
+namespace DATOS
+{
+    public interface IRepositorioProducto
+    {
+            public void Registrar(Producto producto);
+            public Producto EncontrarProducto(string referencia);
+            public List<Producto> EncontrarProductosPorEstado(string estado);
+            public List<Producto> EncontrarProductosPornombre(string nombre);
+
+            public void EliminarProducto(Producto producto);
+        
+
+    }
+}
