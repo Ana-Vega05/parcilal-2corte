cae325dc637b3d43b9d6b7b70aa7e6ad08836734
Dyelamo <dyelamo@unicesar.edu.co>
2024-05-13 19:44:20 -0500

    Cree clase Producto y Enun

---
+++ ENTIDADES/Producto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTIDADES
+{
+    public class Producto
+    {
+
+        
+            public string Referencia { get; set; }
+            public string Nombre { get; set; }
+            public int Existencias { get; set; }
+            public int StockMinimo { get; set; }
+            public decimal PrecioUnitario { get; set; }
+
+
+        
+
+
+
+    }
+}
